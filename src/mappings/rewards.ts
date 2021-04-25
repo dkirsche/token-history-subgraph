21cce2272a778d1a876f73fef9a91e4551987956
Daniel Kirsche <dkirsche@Daniels-MacBook-Pro.local>
2021-04-25 10:51:28 -0500

    fixed broken relationship to asset entity. adjust startBlock so that it begins indexing guage blocks immediately

--- src/mappings/rewards.ts
+++ src/mappings/rewards.ts
@@ -9,8 +9,8 @@ import { BigDecimal, BigInt, Address, Bytes,log } from "@graphprotocol/graph-ts"
 
 export function handleCurveAaveGaugeUpdate(event: UpdateLiquidityLimit): void {
   let gaugeAddress = event.address;
-  let gaugeController = Address.fromString("0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB")
-  let asset = "0xDeBF20617708857ebe4F679508E7b7863a8A8EeE";
+  let gaugeController = Address.fromString("0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB");
+  let asset = Address.fromString("0xDeBF20617708857ebe4F679508E7b7863a8A8EeE");
   let rewardToken = "CRV" //this will need to be updated to be dynamic
   let workingSupply = event.params.working_supply
 
@@ -20,7 +20,7 @@ export function handleCurveAaveGaugeUpdate(event: UpdateLiquidityLimit): void {
   createRewardHistory(asset,gaugeAddress,gaugeController,rewardToken,workingSupply,timestamp,txnHash)
 }
 
-function createRewardHistory(vaultID: String, gaugeAddress: Address, gaugeControllerAddress: Address, rewardToken: String, workingSupply:BigInt, timestamp: BigInt, txnHash: Bytes): RewardHistoryDaily {
+function createRewardHistory(vaultID: Address, gaugeAddress: Address, gaugeControllerAddress: Address, rewardToken: String, workingSupply:BigInt, timestamp: BigInt, txnHash: Bytes): RewardHistoryDaily {
   let gaugeContract = CurveAaveGaugeContract.bind(gaugeAddress);
   let gaugeController = CurveGaugeController.bind(gaugeControllerAddress);
 
@@ -28,7 +28,7 @@ function createRewardHistory(vaultID: String, gaugeAddress: Address, gaugeContro
   let rewardHistoryID = adjustedTimestamp.toString() + gaugeContract.name();
   let dailyReward = RewardHistoryDaily.load(rewardHistoryID);
   if (dailyReward != null) {
-    log.info('Duplicate - txnHash:{} ,timeStamp:{}, calculatedTimestamp:{}', [
+    log.info('Duplicate - txnHash:{} ,timeStamp:{}, rewardHistoryID:{}', [
       txnHash.toHexString(),
       timestamp.toString(),
       rewardHistoryID,
@@ -40,7 +40,7 @@ function createRewardHistory(vaultID: String, gaugeAddress: Address, gaugeContro
   let gaugeDailyReward= gaugeContract.inflation_rate() * BigInt.fromI32(86400) * relative_weight //inflation rate is per second convert to day
 
   dailyReward = new RewardHistoryDaily(rewardHistoryID);
-  dailyReward.asset = vaultID;
+  dailyReward.asset = vaultID.toHexString();
   dailyReward.gaugeId = gaugeAddress;
   dailyReward.rewardPerShareBoosted = new BigDecimal(gaugeDailyReward) / new BigDecimal(workingSupply)
   dailyReward.rewardPerShareNotBoosted = new BigDecimal(gaugeDailyReward) / new BigDecimal(workingSupply) / BigDecimal.fromString("2.5")
