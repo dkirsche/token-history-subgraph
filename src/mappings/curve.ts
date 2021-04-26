20eacca60447ebfaec091355412435c5ff749f28
Dmitri Skjorshammer <dmitriskj@gmail.com>
2021-04-26 12:29:26 -0700

    Add CurveSUSD.

--- src/mappings/curve.ts
+++ src/mappings/curve.ts
@@ -1,5 +1,8 @@
-import { RemoveLiquidity, Curve3Pool }    from "../../generated/curve_3pool/Curve3Pool";
-import { RemoveLiquidity, CurveCompound } from "../../generated/curve_compound/CurveCompound";
+import { RemoveLiquidity }    from "../../generated/curve_3pool/Curve3Pool";
+import { Curve3Pool }    from "../../generated/curve_3pool/Curve3Pool";
+import { CurveCompound } from "../../generated/curve_compound/CurveCompound";
+import { CurveUSDP } from "../../generated/curve_usdp/CurveUSDP";
+import { CurveSUSD } from "../../generated/curve_susd/CurveSUSD";
 import { createPriceHistory } from "../utils/helpers"
 
 export function handleCurve3PoolTransfer(event: RemoveLiquidity): void {
@@ -30,8 +33,20 @@ export function handleCurveCompoundTransfer(event: RemoveLiquidity): void {
 
 export function handleCurveUSDPTransfer(event: RemoveLiquidity): void {
   let vaultAddress  = event.address;
-  let vaultContract = CurveCompound.bind(vaultAddress);
+  let vaultContract = CurveUSDP.bind(vaultAddress);
   let vaultName     = "curve_usdp";
+
+  let timestamp         = event.block.timestamp
+  let txnHash           = event.transaction.hash
+  let totalSupply       = event.params.token_supply;
+  let pricePerFullShare = vaultContract.get_virtual_price();
+  createPriceHistory(vaultAddress, vaultName, totalSupply, pricePerFullShare, timestamp, txnHash)
+}
+
+export function handleCurveSUSDTransfer(event: RemoveLiquidity): void {
+  let vaultAddress  = event.address;
+  let vaultContract = CurveSUSD.bind(vaultAddress);
+  let vaultName     = "curve_susd";
 
   let timestamp         = event.block.timestamp
   let txnHash           = event.transaction.hash
