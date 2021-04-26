678385e5ded691c7cd551b49f9657e273c492158
Dmitri Skjorshammer <dmitriskj@gmail.com>
2021-04-26 12:31:20 -0700

    Adds CurveRen subgraph.

--- src/mappings/curve.ts
+++ src/mappings/curve.ts
@@ -3,6 +3,7 @@ import { Curve3Pool }    from "../../generated/curve_3pool/Curve3Pool";
 import { CurveCompound } from "../../generated/curve_compound/CurveCompound";
 import { CurveUSDP } from "../../generated/curve_usdp/CurveUSDP";
 import { CurveSUSD } from "../../generated/curve_susd/CurveSUSD";
+import { CurveRen } from "../../generated/curve_ren/CurveRen";
 import { createPriceHistory } from "../utils/helpers"
 
 export function handleCurve3PoolTransfer(event: RemoveLiquidity): void {
@@ -46,6 +47,19 @@ export function handleCurveUSDPTransfer(event: RemoveLiquidity): void {
 export function handleCurveSUSDTransfer(event: RemoveLiquidity): void {
   let vaultAddress  = event.address;
   let vaultContract = CurveSUSD.bind(vaultAddress);
+  let vaultName     = "curve_susd";
+
+  let timestamp         = event.block.timestamp
+  let txnHash           = event.transaction.hash
+  let totalSupply       = event.params.token_supply;
+  let pricePerFullShare = vaultContract.get_virtual_price();
+  createPriceHistory(vaultAddress, vaultName, totalSupply, pricePerFullShare, timestamp, txnHash)
+}
+
+
+export function handleCurveRenTransfer(event: RemoveLiquidity): void {
+  let vaultAddress  = event.address;
+  let vaultContract = CurveRen.bind(vaultAddress);
   let vaultName     = "curve_susd";
 
   let timestamp         = event.block.timestamp
