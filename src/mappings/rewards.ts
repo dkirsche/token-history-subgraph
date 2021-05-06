879e7bf57465ec0058e32639634b3c2f03afb4fa
Daniel Kirsche <dkirsche@Daniels-MacBook-Pro.local>
2021-05-06 13:51:22 -0500

    add matic rewards to aave pool on matic

--- src/mappings/rewards.ts
+++ src/mappings/rewards.ts
@@ -1,16 +1,20 @@
 import { UpdateLiquidityLimit } from "../../generated/Curve_aave_gauge/CurveAaveGaugeContract";
 import { CurveAaveContract } from "../../generated/Curve_aave/CurveAaveContract";
 import { CurveAaveGaugeContract } from "../../generated/Curve_aave_gauge/CurveAaveGaugeContract";
+import { CurveRewardsOnlyGauge } from "../../generated/Curve_aave_gauge_matic/CurveRewardsOnlyGauge";
+import { Deposit } from "../../generated/Curve_aave_gauge_matic/CurveRewardsOnlyGauge";
 import { CurveGaugeController } from "../../generated/Curve_aave_gauge/CurveGaugeController";
-import { Asset, RewardHistoryDaily } from "../../generated/schema";
+import { Asset, RewardHistoryDaily, RewardOther } from "../../generated/schema";
 import { roundToDay } from "../utils/helpers";
 import { BigDecimal, BigInt, Address, Bytes,log } from "@graphprotocol/graph-ts";
 
-export function handleCurveAaveGaugeUpdate_matic(event: UpdateLiquidityLimit): void {
+export function handleCurveAaveGaugeUpdate_matic(event: Deposit): void {
   let vault = Address.fromString("0x445FE580eF8d70FF569aB36e80c647af338db351"); //address of vault that uses this guage
-  let rewardToken = "MATIC" //this will need to be updated to be dynamic
-
-  createRewardHistory(event,vault,rewardToken)
+  let rewardToken = "CRV" //this will need to be updated to be dynamic
+  log.info('handleCurveAaveGaugeUpdate_matic was executed',[])
+//if rewards contract is not 0 then load the rewards gaugeContract
+//save that to additional rewards entity
+  createOtherRewardHistory(event,vault,rewardToken)
 }
 export function handleCurveAaveGaugeUpdate(event: UpdateLiquidityLimit): void {
   let vault = Address.fromString("0xDeBF20617708857ebe4F679508E7b7863a8A8EeE"); //address of vault that uses this guage
@@ -67,7 +71,7 @@ function createRewardHistory(event: UpdateLiquidityLimit, vaultID: Address, rewa
   let gaugeController = CurveGaugeController.bind(gaugeControllerAddress);
 
   let adjustedTimestamp = roundToDay(timestamp)
-  let rewardHistoryID = adjustedTimestamp.toString() + gaugeContract.name();
+  let rewardHistoryID = adjustedTimestamp.toString() + gaugeAddress.toHexString();
   let dailyReward = RewardHistoryDaily.load(rewardHistoryID);
   if (dailyReward != null) {
     log.info('Duplicate - txnHash:{} ,timeStamp:{}, rewardHistoryID:{}', [
@@ -96,4 +100,40 @@ function createRewardHistory(event: UpdateLiquidityLimit, vaultID: Address, rewa
   dailyReward.save();
 
   return dailyReward as RewardHistoryDaily;
+}
+
+function createOtherRewardHistory(event: Deposit, vaultID: Address, rewardToken: String): RewardOther {
+  let gaugeAddress = event.address;
+  let timestamp = event.block.timestamp
+  let txnHash=event.transaction.hash
+  let gaugeContract = CurveRewardsOnlyGauge.bind(gaugeAddress);
+
+  let adjustedTimestamp = roundToDay(timestamp)
+  let dailyID = adjustedTimestamp.toString() + gaugeAddress.toHexString();
+  let additionalReward = RewardOther.load(dailyID);
+  if (additionalReward != null) {
+    log.info('Duplicate_RewardOther - txnHash:{} ,timeStamp:{}', [
+      txnHash.toHexString(),
+      adjustedTimestamp.toString(),
+    ])
+    return additionalReward as RewardOther;
+  }
+  log.info('New_RewardOther - txnHash:{} ,timeStamp:{}', [
+    txnHash.toHexString(),
+    timestamp.toString(),
+  ])
+  additionalReward = new RewardOther(dailyID);
+  additionalReward.asset = vaultID.toHexString();
+  additionalReward.gaugeId = gaugeAddress;
+  additionalReward.rewardIntegral = gaugeContract.reward_integral(gaugeContract.reward_tokens(BigInt.fromI32(0)))
+  additionalReward.totalSupply = gaugeContract.totalSupply()
+  additionalReward.rewardContract = gaugeContract.reward_contract().toHexString()
+  additionalReward.reward = gaugeContract.reward_balances(gaugeContract.reward_tokens(BigInt.fromI32(0)))
+  additionalReward.rewardTokenID = gaugeContract.reward_tokens(BigInt.fromI32(0)).toHexString()
+  additionalReward.timestamp = adjustedTimestamp;
+  additionalReward.txnHash = txnHash;
+
+  additionalReward.save();
+
+  return additionalReward as RewardOther;
 }
